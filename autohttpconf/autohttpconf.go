7cb767c981d2d412a49ea3db0fd6bb7a04f0d23f
qygitadm <qdfuture0532@vip.qq.com>
2024-02-28 22:08:22 +0800

    u

--- autohttpconf/autohttpconf.go
+++ autohttpconf/autohttpconf.go
@@ -45,7 +45,7 @@ func (e *ZeroXsacXhttpStructs) XhttpCheckTable() string  { return "" }
 func (e *ZeroXsacXhttpStructs) XhttpSearchIndex() string { return "" }
 
 func (e *ZeroXsacXhttpStructs) XhttpAutoProc() processors.ZeroXsacAutoProcessor {
-	return processors.NewXsacPostgresProcessor(e.XsacDbName(), e.XsacTableName())
+	return processors.NewXsacPostgresProcessor(e.This().(structs.ZeroXsacDeclares).XsacDbName(), e.This().(structs.ZeroXsacDeclares).XsacTableName())
 }
 
 func (e *ZeroXsacXhttpStructs) XhttpFetchTrigger() ZeroXsacHttpFetchTrigger   { return nil }
